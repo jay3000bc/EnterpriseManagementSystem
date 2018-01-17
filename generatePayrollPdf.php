3de90da02f40712c60b78da9e94b4b26b7eb7959
Mukesh Goswami <goswamim654@gmail.com>
2018-01-17 17:41:32 +0530

    paysilip spelling corrected

--- generatePayrollPdf.php
+++ generatePayrollPdf.php
@@ -44,7 +44,7 @@ $resultgetCutOffStatus = $evaluateDaysManager->getCutOffStatus();
 				<img src="images/logo.png" style="width: 30%;" class="img-responsive generate-payroll-logo"><br>
 			</div>
 			<div class="col-md-12">
-				<h4>Pay Silip for the Month of <?php echo date("F Y",strtotime("-1 month"));?><br> Financial year 2017 - 2018</h4><br>
+				<h4>Payslip for the Month of <?php echo date("F Y",strtotime("-1 month"));?><br> Financial year 2017 - 2018</h4><br>
 			</div>
 			<div class="col-md-12">
 				<h4><label>Private &amp; Confidential</label></h4>
