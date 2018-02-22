4576cf912f9edc6ca65cec362fa89daa0552dffd
Jay J. Das <jay3000bc@gmail.com>
2018-02-22 18:44:23 +0530

    removed error reporting

--- PayrollController.php
+++ PayrollController.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 session_start();
 include('settings/config.php');
 date_default_timezone_set('Asia/Kolkata');
